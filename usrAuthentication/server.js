c10560f6f4da2964708ad59eaf5597d04d8c20b1
Jason <jcalle0000@gmail.com>
2020-08-13 14:55:55 -0400

    Successfull Authentication

--- usrAuthentication/server.js
+++ usrAuthentication/server.js
@@ -12,30 +12,48 @@ const users=[
 
 app.get('/users', (req,res)=>{
     res.json(users)
-    console.log('Acessing http://localhost/users')
+    console.log('get-http://localhost/users')
 })
 
 // Using bycrypt which makes the function async
 app.post('/users', async (req,res)=> {
     try{
-        
-        const salt=await bcrypt.genSalt() // bycrypt is async so await is needed
-        const hashedPassword=await bcrypt.hash(req.body.password,salt)
-        console.log(salt)
-        console.log(hashedPassword)  
-        // Our user info being saved
+        // const salt=await bcrypt.genSalt() // bycrypt is async so await is needed
+        const hashedPassword=await bcrypt.hash(req.body.password,10)
         const user={name: req.body.name,
         password: hashedPassword} 
         users.push(user)
         res.status(201).send() // created status response
-        console.log('Posted')
+        console.log('Post '+user.name)
     }
     catch{
-        console.log('Error')
+        console.log('Error Posting in /users')
         res.status(500).send() // generic errpr internal Server Error
     }
   
     // users is our array of data
-    
 })
+
+app.post('/users/login', async (req,res)=>{
+    const user=users.find(user=>user.name===req.body.name);
+    console.log('post/login:user', user);
+    if(user==null){
+        console.log('post user/login: User does not exist')
+        return res.status.send('post/login:user: Cannot find user')
+    }
+    try{
+        if(await bcrypt.compare(req.body.password, user.password)){
+            console.log('post user/login:Successful find')
+            return res.status(400).send('Succesful Login')
+        }
+        else{
+            res.send('Not allowed')
+        }
+    }
+    catch{
+        console.log('User was not verified')
+        res.status(500).send()
+    }
+} )
+
 app.listen(3100);
